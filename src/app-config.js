8f59c7c8b8f7441f57a7d812aebd9e5df26f590a
harvey <alstjr971@naver.com>
2025-02-06 18:45:32 +0900

    deploy

--- src/app-config.js
+++ src/app-config.js
@@ -3,8 +3,10 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
-  // backendHost = "http://localhost:8080";
-  backendHost = "http://harvey-todo-api-service.ap-northeast-2.elasticbeanstalk.com";
+  backendHost = "http://localhost:8080";
+}
+else{
+  backendHost = "https://api.mayfifth99.store";
 }
 
 export const API_BASE_URL = `${backendHost}`;
